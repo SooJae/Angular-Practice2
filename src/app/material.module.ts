ca9e06590732585e76b93bf5fd3d131c041ccf07
soojae <developersoojae@gmail.com>
2020-06-23 21:14:22 +0900

    [기능] 로그인 밸리데이션 추가

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -7,7 +7,7 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatMomentDateModule} from '@angular/material-moment-adapter';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatSidenavModule} from '@angular/material/sidenav';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   imports: [
     MatButtonModule,
@@ -17,7 +17,8 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     MatDatepickerModule,
     MatMomentDateModule,
     MatCheckboxModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule
   ],
   exports: [
     MatButtonModule,
@@ -27,7 +28,8 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     MatDatepickerModule,
     MatMomentDateModule,
     MatCheckboxModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule {
